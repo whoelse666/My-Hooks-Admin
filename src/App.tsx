53e69753df4f9d88b003897aa8c7b495849d451e
pengrongwei <347300979@qq.com>
2023-08-30 11:41:34 +0800

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,50 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import { Layout, Space, Button } from 'antd';
+import { useState, useEffect } from "react";
+import { getBrowserLang } from "@/utils/util";
+import { ConfigProvider } from "antd";
+import { setLanguage } from "@/redux/modules/global";
+import { HashRouter } from "react-router-dom";
+import { RootState, useDispatch, useSelector } from "@/redux";
+import AuthRouter from "@/routers/utils/authRouter";
+import Router from "@/routers/index";
+import useTheme from "@/hooks/useTheme";
+import zhCN from "antd/lib/locale/zh_CN";
+import enUS from "antd/lib/locale/en_US";
+import i18n from "i18next";
+import "moment/dist/locale/zh-cn";
 
-const { Header, Footer, Sider, Content } = Layout;
-const headerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  height: 64,
-  paddingInline: 50,
-  lineHeight: '64px',
-  backgroundColor: '#7dbcea',
+const App = () => {
+	const dispatch = useDispatch();
+	const { language, assemblySize } = useSelector((state: RootState) => state.global);
+	const [i18nLocale, setI18nLocale] = useState(zhCN);
+
+	// 全局使用主题
+	useTheme();
+
+	// 设置 antd 语言国际化
+	const setAntdLanguage = () => {
+		// 如果 redux 中有默认语言就设置成 redux 的默认语言，没有默认语言就设置成浏览器默认语言
+		if (language && language == "zh") return setI18nLocale(zhCN);
+		if (language && language == "en") return setI18nLocale(enUS);
+		if (getBrowserLang() == "zh") return setI18nLocale(zhCN);
+		if (getBrowserLang() == "en") return setI18nLocale(enUS);
+	};
+
+	useEffect(() => {
+		// 全局使用国际化
+		i18n.changeLanguage(language || getBrowserLang());
+		dispatch(setLanguage(language || getBrowserLang()));
+		setAntdLanguage();
+	}, [language]);
+
+	return (
+		<HashRouter>
+			<ConfigProvider locale={i18nLocale} componentSize={assemblySize}>
+				<AuthRouter>
+					<Router />
+				</AuthRouter>
+			</ConfigProvider>
+		</HashRouter>
+	);
 };
 
-const contentStyle: React.CSSProperties = {
-  textAlign: 'center',
-  minHeight: 120,
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#108ee9',
-};
-
-const siderStyle: React.CSSProperties = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#3ba0e9',
-};
-
-const footerStyle: React.CSSProperties = {
-  textAlign: 'center',
-  color: '#fff',
-  backgroundColor: '#7dbcea',
-};
-const rootboxStyle: React.CSSProperties = {
-  height: "100vh",
-}
-
-function App() {
-
-  return (
-    <Layout className="root-box" style={rootboxStyle}>
-      <Sider style={siderStyle}>Sider</Sider>
-      <Layout>
-        <Header style={headerStyle}>Header</Header>
-        <Content style={contentStyle}>Content</Content>
-        <Footer style={footerStyle}>Footer</Footer>
-      </Layout>
-    </Layout>
-  )
-}
-
-export default App
+export default App;
